4abbd849fa67a2ff428475012053b88ec2eb47e2
medvedium <mumbles2606@gmail.com>
2021-03-08 04:05:07 +0300

    Done!

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,20 @@
-@@include('bootstrap.bundle.min.js');
-
 document.addEventListener('DOMContentLoaded', () => {
+  const mobileMenu = document.querySelector('.header__nav--mobile'),
+    mobileLink = mobileMenu.querySelectorAll('.nav__list--link'),
+    burger = document.querySelector('.burger'),
+    body = document.querySelector('body');
 
+  mobileLink.forEach(item => {
+    item.addEventListener('click', () => {
+      burger.classList.toggle('active');
+      body.classList.toggle('lock');
+      mobileMenu.classList.toggle('active');
+    });
+  });
+
+  burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    mobileMenu.classList.toggle('active');
+    body.classList.toggle('lock');
+  });
 });
